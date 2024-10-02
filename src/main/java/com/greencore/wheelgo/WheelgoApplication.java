2ced7d01bdb0df157dd737197c482cc3b85e6cef
ingyeomnote <freeartjin@naver.com>
2024-10-02 23:49:15 +0900

    spring initializr

---
+++ src/main/java/com/greencore/wheelgo/WheelgoApplication.java
@@ -0,0 +1,13 @@
+package com.greencore.wheelgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheelgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheelgoApplication.class, args);
+	}
+
+}
